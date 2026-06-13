a12a33a07ec7cecb4d9778d115c3ecfabc5baf6e
Arsy <muhammadarsyalfahd@gmail.com>
2026-06-13 22:34:53 +0800

    feat: add favicon and web manifest files for improved branding and user experience

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Error' }} - SuaraSosial</title>
+    @include('partials.favicon')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
